43a1b2f33f08ca2525239df9e4775b02f36b4847
zerogon <safoma@naver.com>
2024-05-13 16:24:44 +0900

    first commit

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,9 @@
+with DAG(
+    dag_id="example_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["example", "example2"],
+    params={"example_key": "example_value"},
+) as dag:
