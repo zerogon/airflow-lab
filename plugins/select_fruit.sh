7fa46d9c1eeefd165d3d3b604fe0c0528d64808e
zerogon <safoma@naver.com>
2024-05-30 18:31:37 +0900

    ad shell

---
+++ plugins/select_fruit.sh
@@ -0,0 +1,10 @@
+FRUIT=$1
+if [ $FRUIT == APPLE ];then
+    echo "Apple"
+elif [ $FRUIT == ORANGE ];then
+    echo "Orange"
+elif [ $FRUIT == GRAPE ];then
+    echo "grape"
+else
+    echo "other fruit"
+fi
