4f68d0a1f59dd03035b01e9eaa490da5fd137541
zerogon <safoma@naver.com>
2024-06-03 19:25:13 +0900

    python operator

---
+++ dags/dags_python_operator.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import datetime
+
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.operators.python import PythonOperator
+import random
+
+with DAG(
+    dag_id="dags_python_operator",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    def select_fruit():
+        fruit = ['APPLE','BANANA','ORANGE','AVOCADO']
+        rand_int = random.randint(0,3)
+        print(fruit[rand_int])
+    
+    py_t1 = PythonOperator(
+        task_id = 'py_t1',
+        python_callable = select_fruit
+    )
+
+    py_t1
