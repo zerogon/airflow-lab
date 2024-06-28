b8ce6f883c8e9015a078a537bbb6792957531d57
zerogon <safoma@naver.com>
2024-06-28 20:29:21 +0900

    add xcom email

--- dags/args/kwargs.py
+++ dags/args/kwargs.py
@@ -22,3 +22,15 @@ with DAG(
         op_kwargs={'email' : 'ss@na.com', 'phone' : '010'}
     )
 regist2_t1
+'''
+def regist2(name, sex, *args,**kwargs):
+    print(f'이름: {name}')
+    print(f'성별: {sex}')
+    print(f'기타옵션: {args}')
+    email = kwargs['email'] or None
+    phone = kwargs['phone'] or None
+    if email :
+        print(email)
+    if phone :
+        print(phone)
+'''
